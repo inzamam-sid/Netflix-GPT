1c5c824472fa3da8650dc8100194d039fa7102a2
inzamam-siddiqui <siddiquiinzmam319@gmail.com>
2026-01-02 19:36:00 +0530

    feat: integrate user profile update on sign-up and dispatch user data to Redux store

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,14 +2,17 @@ import React, { useState, useRef } from 'react'
 import { useNavigate } from 'react-router-dom';
 import Header from './Header'
 import { checkValidateData } from '../utils/validate';
-import {createUserWithEmailAndPassword, signInWithEmailAndPassword} from "firebase/auth";
+import {createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile} from "firebase/auth";
 import { auth } from "../utils/firebase";
+import { useDispatch } from 'react-redux';
+import { addUser } from '../utils/userSlice';
  
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
   const [errorMessage, setErrorMessage] = useState(null);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const name = useRef(null);
   const email = useRef(null);
@@ -37,8 +40,26 @@ const Login = () => {
       ).then((userCredential) => {
         // Signed in
         const user = userCredential.user;
-        console.log(user);
-        navigate('/browse');
+
+        updateProfile(user, {
+            displayName: name.current.value, photoURL: "https://avatars.githubusercontent.com/u/165045062?v=4"
+          }).then(() => {
+            
+              const {uid, email, displayName, photoURL} = auth.currentUser;
+                    dispatch(addUser({
+                      uid: uid,
+                      email: email,
+                      displayName: displayName,
+                      photoURL: photoURL
+                    }))
+
+            navigate('/browse');
+          }).catch((error) => {
+            setErrorMessage(error.message);
+          });
+
+        // console.log(user);
+        // navigate('/browse');
       })
       .catch((error) => {
           const errorCode = error.code;
